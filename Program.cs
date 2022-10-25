bcb96c0137c2cdbbef935242c425c96a02b784a9
KozlovOleg97 <kozlovoleg2021@gmail.com>
2022-10-25 16:39:37 +0300

    Implementation Rabbit's class

--- Program.cs
+++ Program.cs
@@ -8,9 +8,12 @@ Animal leopard = new Leopard("Leopard");
 
 Animal tiger = new Tiger("Tiger");
 
+Animal rabbit = new Rabbit("Rabbit");
+
 horse.Move();
 leopard.Move();
 tiger.Move();
+rabbit.Move();
 
 
 
