d28fce95b80573ab841bae6621fc1b5e26ab4d60
KozlovOleg97 <kozlovoleg2021@gmail.com>
2022-10-25 16:29:11 +0300

    Adding Rabbit class

--- Program.cs
+++ Program.cs
@@ -8,9 +8,13 @@ Animal leopard = new Leopard("Leopard");
 
 Animal tiger = new Tiger("Tiger");
 
+Animal rabbit = new Rabbit("Rabbit");
+
 horse.Move();
 leopard.Move();
 tiger.Move();
+rabbit.Move();
+
 
 
 
